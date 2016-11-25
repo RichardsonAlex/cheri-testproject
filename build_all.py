7ba88488408b5bf28364062dd284d56cb0ae94e3
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2016-11-25 21:01:54 +0000

    Add build_all.py script

---
+++ build_all.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import subprocess
+import shutil
+from pathlib import Path
+
+
+def run(cmd: list, cwd: Path):
+    print("cd ", cwd, "&&", " ".join(cmd))
+    subprocess.check_call(cmd, cwd=str(cwd))
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--clean", help="increase output verbosity",
+                    action="store_true")
+args = parser.parse_args()
+
+
+variants = ["dynamic", "dynamic-with-lld", "static", "static-with-lld"]
+srcdir = Path(__file__).parent
+for variant in variants:
+    builddir = srcdir / (os.uname().sysname + "-" + variant)  # type: Path
+    if args.clean and (builddir / "CMakeCache.txt").exists():
+        print("rm -rf", builddir)
+        # shutil.rmtree(str(builddir))
+    if not builddir.exists():
+        os.makedirs(str(builddir), exist_ok=True)
+    run(["cmake-for-cheribsd-cheriabi-" + variant + ".sh", "-GNinja", "-DCMAKE_BUILD_TYPE=Debug", ".."], cwd=builddir)
+    run(["ninja"], cwd=builddir)
+
