0bded4a10397d089ffd72cf0cc7c3d4f15d95fde
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2016-11-25 18:15:32 +0000

    Add script to build for all architectures

---
+++ rebuild-all.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+clean_and_compile() {
+	test -f "$1/CMakeCache.txt" && rm -r "$1"
+	mkdir -p "$1"
+	pushd "$1"
+	command "cmake-for-cheribsd-cheriabi-$1.sh" -G Ninja .. -DCMAKE_BUILD_TYPE=Release
+	ninja
+	popd
+}
+
+clean_and_compile dynamic
+clean_and_compile dynamic-with-lld
+clean_and_compile static
+clean_and_compile static-with-lld
