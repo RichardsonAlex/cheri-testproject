79608881fadeba2ff448a834fadef989b6205685
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2016-11-26 00:08:33 +0000

    Don't cause a length violation

--- helloworld.c
+++ helloworld.c
@@ -3,6 +3,7 @@
 extern void* _etext;
 
 int main() {
-    printf("Hello, World %p!\n", _etext);
+    printf("Hello, World!\n");
+    // FIXME: this cause a length violation: printf("_etext = %p\n", _etext);
     return 0;
 }
